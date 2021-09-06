9b9fe4707969cf0850a855cf6397bddde3b958dc
Joseph Williams <josephwilliamsjs@gmail.com>
2021-09-06 13:19:37 -0700

    First section

---
+++ rspec-course/spec/card_spec.rb
@@ -0,0 +1,33 @@
+# code
+class Card
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+
+# specs
+RSpec.describe Card do
+  let(:card) { Card.new("Ace", "Spades") }
+
+
+  it "has a rank" do
+    expect(card.rank).to eq("Ace")
+    card.rank = "Queen"
+    expect(card.rank).to eq("Queen")
+
+  end
+
+  it "has a suit" do
+    expect(card.suit).to eq("Spades")
+  end
+
+  it "has a custom error message" do
+    comparison = "Spade"
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
+  end
+end
+
